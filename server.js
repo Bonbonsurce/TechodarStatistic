2068edc44cad82feea42881c39a36f0113f52e70
bonbonsurce <aleksei.cherehovich@mail.ru>
2024-01-31 21:23:53 +0300

    new_fyles

--- server.js
+++ server.js
@@ -2,6 +2,41 @@ const http = require('node:http');
 const fs = require("fs");
 const hostname = '127.0.0.1';
 const port = 3000;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'technodarstatistic',
+    password: 'com4ohCe',
+    port: 5432, // порт по умолчанию для PostgreSQL
+});
+
+//Проверка подключения к бд
+// Обработчик события "error" для отслеживания ошибок подключения
+pool.on('error', (err) => {
+    console.error('Ошибка подключения к базе данных:', err);
+});
+
+// Обработчик события "connect" для отслеживания успешного подключения
+pool.on('connect', () => {
+    console.log('Подключение к базе данных успешно!');
+});
+
+// Попытка подключения к базе данных
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Ошибка получения клиента из пула:', err);
+    }
+    console.log('Клиент успешно получен из пула');
+
+    // Здесь можно выполнять запросы к базе данных
+
+    // Освобождаем клиента обратно в пул
+    release();
+});
+
+
 const server = http.createServer((req, res) => {
     let filePath = __dirname + '/public/index.html';
 
@@ -11,11 +46,23 @@ const server = http.createServer((req, res) => {
         case '/ship':
             filePath = __dirname + '/public/ship.html';
             break;
+        case '/ship/add':
+            filePath = __dirname + '/public/ship_add.html';
+            break;
         case '/equipment':
             filePath = __dirname + '/public/equipment.html';
             break;
+        case '/equipment/add':
+            filePath = __dirname + '/public/equipment_add.html';
+            break;
+        case '/equipment/add_type':
+            filePath = __dirname + '/public/add_type_eq.html';
+            break;
         case '/projects':
             filePath = __dirname + '/public/projects.html';
+            break;
+        case '/projects/add':
+            filePath = __dirname + '/public/projects_add.html';
             break;
         default:
             filePath = __dirname + '/public/index.html';
